0fcf270392641b882f656f2f9b76aa81b4b34777
Mahmoud Mostafa Awad <mahmoudmostafa121121@gmail.com>
2025-11-25 14:19:36 +0200

    .

---
+++ src/main/java/com/springdemo/pulsegym/PulseGymApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.pulsegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseGymApplication.class, args);
+    }
+
+}
