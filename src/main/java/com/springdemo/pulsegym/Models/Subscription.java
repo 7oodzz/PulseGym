1c23bb653f1712da3469123182b5b489b886e638
farah-samy <farahsami12@gmail.com>
2025-11-28 19:46:08 +0200

    subscription

--- src/main/java/com/springdemo/pulsegym/Models/Subscription.java
+++ src/main/java/com/springdemo/pulsegym/Models/Subscription.java
@@ -1,11 +1,69 @@
 package com.springdemo.pulsegym.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 import java.util.Date;
 
+@Entity
 public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     private String name;
-    private String price;
+    private double price;
     private String description;
     private Date startDate;
     private Date expDate;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getExpDate() {
+        return expDate;
+    }
 }
