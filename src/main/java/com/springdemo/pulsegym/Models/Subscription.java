ac9df11d859cb24a18554cada2e1528e6a8fe98a
farah-samy <farahsami12@gmail.com>
2025-11-28 19:22:57 +0200

    ayooo

---
+++ src/main/java/com/springdemo/pulsegym/Models/Subscription.java
@@ -0,0 +1,11 @@
+package com.springdemo.pulsegym.Models;
+
+import java.util.Date;
+
+public class Subscription {
+    private String name;
+    private String price;
+    private String description;
+    private Date startDate;
+    private Date expDate;
+}
