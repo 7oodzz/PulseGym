1f4b9abafab3a340be89d9894e6e387b809f7fbd
NoteBook <merly_20235121@fci.helwan.edu.eg>
2025-11-29 17:32:01 +0200

    hmmm

--- src/main/java/com/springdemo/pulsegym/Service/ReceptionistService.java
+++ src/main/java/com/springdemo/pulsegym/Service/ReceptionistService.java
@@ -2,8 +2,13 @@ package com.springdemo.pulsegym.Service;
 
 import com.springdemo.pulsegym.Model.Receptionist;
 import com.springdemo.pulsegym.Repository.ReceptionistRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 
+@Service
 public class ReceptionistService {
 
     @Autowired
@@ -14,22 +19,28 @@ public class ReceptionistService {
         this.receptionistRepo = receptionistRepo;
     }
 
-    public Receptionist addReceptionist(String name, String ssn) {
+    public Receptionist addReceptionist(String name ,String ssn) {
+        if(receptionistRepo.findBySsn(ssn).isPresent()){
+            throw new IllegalStateException("Receptionist is already exist with this ssn "+ssn);
+        }
         Receptionist rec = new Receptionist();
         rec.setName(name);
         rec.setSsn(ssn);
         return receptionistRepo.save(rec);
     }
 
-    public Receptionist updateReceptionist(String id, Receptionist updated) {
-        Receptionist original = receptionistRepo.findById(id)
+    public Receptionist updateReceptionist(String ssn, Receptionist updated) {
+        Receptionist original = receptionistRepo.findBySsn(ssn)
                 .orElseThrow(() -> new RuntimeException("Member not found"));
         original.setName(updated.getName());
         original.setSsn(updated.getSsn());
         return receptionistRepo.save(original);
     }
 
-    public void deleteReceptionist(String receptionistId) {
-        receptionistRepo.deleteById(receptionistId);
+    public void deleteReceptionist(String ssn) {
+        if(!receptionistRepo.findBySsn(ssn).isPresent()){
+            throw new IllegalStateException("Receptionist does not exist");
+        }
+        receptionistRepo.deleteBySsn(ssn);
     }
 }
