c84d136ef861c3423426df9261c87365b1377ab4
NoteBook <merly_20235121@fci.helwan.edu.eg>
2025-11-29 17:32:01 +0200

    hmmm

--- src/main/java/com/springdemo/pulsegym/Repository/ReceptionistRepository.java
+++ src/main/java/com/springdemo/pulsegym/Repository/ReceptionistRepository.java
@@ -1,6 +1,13 @@
 package com.springdemo.pulsegym.Repository;
 
 import com.springdemo.pulsegym.Model.Receptionist;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ReceptionistRepository extends JpaRepository<Receptionist,Long> {
+import java.util.Optional;
+
+public interface ReceptionistRepository extends JpaRepository<Receptionist,Long> {
+
+    Optional<Receptionist> findBySsn(String ssn);
+    Optional<Receptionist> deleteBySsn(String ssn);
+
 }
