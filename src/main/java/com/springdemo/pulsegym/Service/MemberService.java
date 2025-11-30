e950f31ee05b6e2a6ddfc91e8944c05e87f6624b
veroo <verooedward@gmail.com>
2025-11-30 02:16:55 +0200

    updated member controller, service, repo

--- src/main/java/com/springdemo/pulsegym/Service/MemberService.java
+++ src/main/java/com/springdemo/pulsegym/Service/MemberService.java
@@ -1,5 +1,21 @@
 package com.springdemo.pulsegym.Service;
 
+import com.springdemo.pulsegym.Model.Member;
+import com.springdemo.pulsegym.Repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
-    
+    @Autowired
+    private MemberRepository memberRepo;
+
+    public Member addMember(Member member){
+        return memberRepo.save(member);
+    }
+    public Member updateMember(Member member){
+        return memberRepo.save(member);
+    }
+
+
 }
