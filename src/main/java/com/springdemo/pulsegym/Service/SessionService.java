9a00e830d67d5e4dde69e192915d235bb3995427
Clara Kamal <clarakamal@gmail.com>
2025-12-01 22:16:29 +0200

    .

--- src/main/java/com/springdemo/pulsegym/Service/SessionService.java
+++ src/main/java/com/springdemo/pulsegym/Service/SessionService.java
@@ -1,5 +1,47 @@
 package com.springdemo.pulsegym.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.springdemo.pulsegym.Model.Session;
+
 public class SessionService {
+       private List<Session> sessions = new ArrayList<>();
+
+   
+    public void addSession(Session session) {
+        sessions.add(session);
+    }
+
+
+    public List<Session> getAllSessions() {
+        return sessions;
+    }
+
+  
+    public Session getSessionById(int id) {
+        Optional<Session> result = sessions
+                .stream()
+                .filter(s -> s.getId() == id)
+                .findFirst();
+        return result.orElse(null);
+    }
+
+   
+    public boolean updateSession(int id, Session updatedSession) {
+        for (int i = 0; i < sessions.size(); i++) {
+            if (sessions.get(i).getId() == id) {
+                updatedSession.setId(id); 
+                sessions.set(i, updatedSession);
+                return true;
+            }
+        }
+        return false;
+    }
+
     
+    public boolean deleteSession(int id) {
+        return sessions.removeIf(s -> s.getId() == id);
+    }
 }
