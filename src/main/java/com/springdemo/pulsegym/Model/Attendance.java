23f8cf5481a6a7756852afd95803bcdb48b32adb
Ghoniem926 <mohamedghoniem.ay@gmail.com>
2025-12-09 17:56:05 +0200

    Attendance methods

--- src/main/java/com/springdemo/pulsegym/Model/Attendance.java
+++ src/main/java/com/springdemo/pulsegym/Model/Attendance.java
@@ -1,5 +1,34 @@
 package com.springdemo.pulsegym.Model;
 
-public class attendance {
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime checkInTime; 
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // Constructors
+    public Attendance() {}
+
+    public Attendance(Member member) {
+        this.member = member;
+        this.checkInTime = LocalDateTime.now(); 
+    }
+
+    
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public LocalDateTime getCheckInTime() { return checkInTime; }
+    public void setCheckInTime(LocalDateTime checkInTime) { this.checkInTime = checkInTime; }
+    public Member getMember() { return member; }
+    public void setMember(Member member) { this.member = member; }
 }
