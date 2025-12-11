b7ccb88d6f2423a3638f42ffe7dc623e65ea911e
Mahmoud Mostafa Awad <mahmoudmostafa121121@gmail.com>
2025-12-12 00:35:12 +0200

    .

--- src/main/java/com/springdemo/pulsegym/Service/AttendanceService.java
+++ src/main/java/com/springdemo/pulsegym/Service/AttendanceService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Service
 @Transactional
-public class AttendanceService{
+public class AttendanceService {
 
     @Autowired
     private AttendanceRepository attendanceRepository;
@@ -23,13 +23,10 @@ public class AttendanceService{
     private MemberRepository memberRepository;
 
     @Autowired
-    private MemberRepository memberRepo;
-
-    @Autowired
     private MemberSessionRepository memberSessionRepository;
 
     public String checkInMember(int memberId) {
-        
+
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new RuntimeException("Member not found!"));
 
@@ -38,20 +35,28 @@ public class AttendanceService{
         }
 
         if(member.getHasSession()) {
-            Optional <MemberSession> sessionBox = memberSessionRepository.findByMemberAndIsActiveTrue(member);
-            MemberSession session = sessionBox.get();
-            session.setSessionsLeft(session.getSessionsLeft() - 1);
-            memberSessionRepository.save(session);
-            if (session.getSessionsLeft() == 0) {
+            // Updated logic based on user input
+            Optional<MemberSession> sessionBox = memberSessionRepository.findByMemberAndSessionsLeftGreaterThan(member, 0);
+
+            if (sessionBox.isEmpty()) {
                 member.setHasSession(false);
-                memberRepo.save(member);
-                memberSessionRepository.deleteByInt(session.getSessionId());
+                memberRepository.save(member);
+            } else {
+                MemberSession session = sessionBox.get();
+                session.setSessionsLeft(session.getSessionsLeft() - 1);
+                memberSessionRepository.save(session);
+
+                if (session.getSessionsLeft() == 0) {
+                    member.setHasSession(false);
+                    memberRepository.save(member);
+                    memberSessionRepository.deleteById(session.getSessionId());
+                }
             }
         }
 
         Attendance attendance = new Attendance(member);
         attendanceRepository.save(attendance);
 
-        return "Check-in successful for member: " + member.getName(); 
+        return "Check-in successful for member: " + member.getName();
     }
 }
